114d064ad9bbb9a95bf750818e412c6ddf6f5f2d
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-05-06 16:14:52 +0300

    add styles

---
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashBoardDeskTopLayout extends StatelessWidget {
+  const DashBoardDeskTopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
