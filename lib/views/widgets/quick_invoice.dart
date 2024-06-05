6bb4f1bee2d1e7a0a6382e3c454b1efea5a30f93
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-06-05 17:59:40 +0300

    custom textfield

--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/custom_text_field.dart';
 import 'package:responsive_dash_board/views/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/views/widgets/quick_invoice_header.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
@@ -13,6 +14,10 @@ class QuikInvoice extends StatelessWidget {
       children: [
         QuikInvoiceHeader(),
         LatestTransaction(),
+        Divider(
+          height: 48,
+        ),
+        CustomTextField()
       ],
     ));
   }
