c97fb808e8dcd2ab94bc8d96b1f78142b330ab35
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-08-18 22:14:20 +0300

    responsive tablet

--- lib/views/widgets/user_info_list_tile.dart
+++ lib/views/widgets/user_info_list_tile.dart
@@ -14,13 +14,21 @@ class UserInfoListTile extends StatelessWidget {
       child: Center(
         child: ListTile(
           leading: SvgPicture.asset(userInfoModel.image),
-          title: Text(
-            userInfoModel.title,
-            style: AppStyles.styleSemiBold16(context),
+          title: FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: AlignmentDirectional.centerStart,
+            child: Text(
+              userInfoModel.title,
+              style: AppStyles.styleSemiBold16(context),
+            ),
           ),
-          subtitle: Text(
-            userInfoModel.subTitle,
-            style: AppStyles.styleRegular12(context),
+          subtitle: FittedBox(
+            alignment: AlignmentDirectional.centerStart,
+            fit: BoxFit.scaleDown,
+            child: Text(
+              userInfoModel.subTitle,
+              style: AppStyles.styleRegular12(context),
+            ),
           ),
         ),
       ),
